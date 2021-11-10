d94a6594f1cc2bb5a24eab684a25caed23819312
meihua <2573727214@qq.com>
2021-11-10 19:30:11 +0800

    init webpack

---
+++ src/index.js
@@ -0,0 +1,36 @@
+// import _ from 'lodash';
+// import printMe from './print.js';
+
+// function component() {
+//     const element = document.createElement('div');
+//     const btn = document.createElement('button');
+
+//     // lodash 在当前 script 中使用 import 引入
+//     element.innerHTML = _.join(['Hello', 'webpack', '中文'], ' ');
+
+//     // btn.innerHTML = 'Click me and check the console!';
+//     // btn.onclick = printMe;
+
+//     // element.appendChild(btn);
+
+//     return element;
+// }
+
+// document.body.appendChild(component());
+
+async function getComponent() {
+    const { default: _ } = await import('lodash');
+    // return import('lodash')
+        // .then(({ default: _ }) => {
+            const element = document.createElement('div')
+
+            element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+            return element;
+        // })
+        // .catch(error => 'An error eccured while loading the component');
+}
+
+getComponent().then((component) => {
+    document.body.appendChild(component);
+});
